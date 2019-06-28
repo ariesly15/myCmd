9e93f08522ab35be475acf43016aa1e6f5e632ae
aweleey <liyidyx@gmail.com>
2019-06-28 14:30:31 +0800

    ts

---
+++ src/commands/extts.ts
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const s = `commands${path.sep}${path.basename(__filename)}`;
+const log = signale.scope(s);
+
+module.exports = {
+    cmd: {
+        name: "extts",
+        usage: "",
+        description: "目录下的文件后缀改为ts",
+        options: [
+            {
+                pattern: "-t, --test",
+                desc: "TEST",
+                default: ""
+            }
+        ]
+    },
+    exec: (pwd, options) => {
+        log.star(pwd, options);
+    }
+};
